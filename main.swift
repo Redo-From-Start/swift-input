5d0d6fcc3e88c5d9a012bf2c7cf09ec2bb267317
Redo From Start <55228370+Redo-From-Start@users.noreply.github.com>
2019-12-28 15:44:43 +0000

    Add files via upload

---
+++ main.swift
@@ -0,0 +1,15 @@
+//
+//  main.swift
+//  input
+//
+//  Created by Benjamin Love Junior on 28/12/2019.
+//  Copyright © 2019 Benjamin Love Junior. All rights reserved.
+//
+
+import Foundation
+
+func input() -> String{
+    let input = readLine();
+    let ret: String = String(input!);
+    return ret;
+}
